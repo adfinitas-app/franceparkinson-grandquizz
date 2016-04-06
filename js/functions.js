859946caef8a23126840420c7158047471ad5556
LionelSpan <lionel.spanneut@gmail.com>
2016-04-07 00:48:04 +0200

    Added files via upload

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 
 function moveScroll() {
 	$('.bt-next, #main .bt').on('click', function(event) {
-		event.stopPropagation();
 		event.preventDefault();
 
 		var sectionName = $(this).attr('href');	
