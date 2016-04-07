924dbd12e6695439860ffabbf7e1148741be3210
LionelSpan <lionel.spanneut@gmail.com>
2016-04-07 22:09:34 +0200

    ajout scroll page merci + rangement fichier

--- js/functions.js
+++ js/functions.js
@@ -2,25 +2,35 @@
 $(document).ready(function() {
 	moveScrollNext();
 	moveScrollQuestion();
-
+	moveScrollThanks();
 });
-
 
 function moveScrollNext() {
 	$('.bt-next').on('click', function(event) {
 		event.preventDefault();
 		event.stopPropagation();
-
 		var sectionName = $(this).attr('href');	
-		var sectionPosition = $(sectionName).offset();
-		$('html, body').animate({scrollTop: (sectionPosition.top + 1)}, 700);
+		animateScroll(sectionName, 700);
 	});
 }
 
 function moveScrollQuestion() {
 	$('#main .bt').on('click', function(event) {
 		var sectionName = $(this).attr('data-href');	
-		var sectionPosition = $(sectionName).offset();
-		$('html, body').animate({scrollTop: (sectionPosition.top + 1)}, 700);
+		animateScroll(sectionName, 700);
 	});
 }
+
+function moveScrollThanks() {
+	if ($('#main').hasClass('page-thanks')) {
+		setTimeout(function(){
+			animateScroll('#bottom', 3000);
+		}, 2000);
+	}
+}
+
+function animateScroll (_sectionName, _timer) {
+	var sectionPosition = $(_sectionName).offset();
+	$('html, body').animate({scrollTop: (sectionPosition.top + 1)}, _timer);
+}
+
